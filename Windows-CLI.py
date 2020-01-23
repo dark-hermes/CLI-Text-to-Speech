6ad188f66a76d1ca5500a6bffc4d635291701eaa
Hermawan Sentyaki <52445704+hermawans@users.noreply.github.com>
2020-01-23 19:35:15 +0700

    Update Windows-CLI.py

--- Windows-CLI.py
+++ Windows-CLI.py
@@ -1,5 +1,7 @@
 from gtts import gTTS
 import os
+from playsound import playsound
+import re
 
 print("""
 ==================
@@ -7,9 +9,21 @@ CLI-Text-to-Speech
   @hermawanzss
 ================== 
 """)
+while True:
+	language = input("\nLanguage (id/en)\t: ")
+
+	if re.search(language,"bahasaidindonesia"):
+		language = "id"
+		break
+	elif re.search(language,"englishinggris"):
+		language = "en"
+		break
+	else:
+		print("\nUnidentified Language\nBahasa and English only")
+	
 while True :
-	text = input("\nText\t: ")
-	tts = gTTS(text, lang='en')
+	text = input("\nText\t\t\t: ")
+	tts = gTTS(text, lang=language)
 	tts.save("{text}.mp3".format(text=text))
-	os.system("vlc \"{text}.mp3\"".format(text=text)) # replace vlc with your audio player (CLI)
+	playsound("{text}.mp3".format(text=text))
 	os.system("del \"{text}.mp3\"".format(text=text))
